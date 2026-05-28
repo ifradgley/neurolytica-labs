97fc4a46419a5e128db2f4d2dcd0d31bb4a5d18d
ifradgley <india.fradgley@gmail.com>
2026-05-28 17:07:57 +0100

    Enhance button styles: update view button with mask and glow effects; adjust padding and border-radius

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,14 @@ function Home() {
               with clarity and confidence.
             </p>
             <NavLink to="/technology">
-              <button Id="view-button">View Technology</button>
+              <button id="view-button">
+                View Technology
+                <div className="view-button-mask">
+                  <div className="view-button-glow"></div>
+                </div>
+              </button>
             </NavLink>
+
             <NavLink to="/publications">
               <button id="explore-button" onClick={handleClick}>
                 Explore Publications
