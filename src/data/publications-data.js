f1b29ef2de9f2be16d531c735fcac8a2b42ef496
ifradgley <india.fradgley@gmail.com>
2026-06-07 19:57:10 +0100

    Publications page: implement table display for research publications

---
+++ src/data/publications-data.js
@@ -0,0 +1,89 @@
+const publications = [
+  {
+    id: 1,
+    title:
+      "Brain manganese and the balance between essential roles and neurotoxicity.",
+    author:
+      "Balachandran, R. C., Mukhopadhyay, S., McBride, D., Veevers, J., Harrison, F. E., Aschner, M., Haynes, E. N., & Bowman, A. B.",
+    date: "2020",
+    link: "https://doi.org/10.1074/jbc.REV119.009453",
+  },
+  {
+    id: 2,
+    title:
+      "Neurotoxicity mechanisms and clinical implications of six common recreational drugs.",
+    author:
+      "Wang, J., Hao, Y., Ma, D., Feng, L., Yang, F., An, P., Su, X., & Feng, J.",
+    date: "2025",
+    link: "https://doi.org/10.3389/fphar.2025.1526270",
+  },
+  {
+    id: 3,
+    title: "Neurotoxicity of metals.",
+    author: "Caito, S., & Aschner, M.",
+    date: "2015",
+    link: "https://doi.org/10.1016/B978-0-444-62627-1.00011-1",
+  },
+  {
+    id: 4,
+    title: "Mechanisms of Cadmium Neurotoxicity.",
+    author: "Arruebarrena, M. A., Hawe, C. T., Lee, Y. M., & Branco, R. C.",
+    date: "2023",
+    link: "https://doi.org/10.3390/ijms242316558",
+  },
+  {
+    id: 5,
+    title:
+      "Neurological Complications of Conventional and Novel Anticancer Treatments",
+    author:
+      "Alberti, P., Salvalaggio, A., Argyriou, A. A., Bruna, J., Visentin, A., Cavaletti, G., & Briani, C.",
+    date: "2022",
+    link: "https://doi.org/10.3390/cancers14246088",
+  },
+  {
+    id: 6,
+    title: "Epigenetic influence of environmentally neurotoxic metals.",
+    author:
+      "Ijomone, O. M., Ijomone, O. K., Iroegbu, J. D., Ifenatuoha, C. W., Olung, N. F., & Aschner, M.",
+    date: "2020",
+    link: "https://doi.org/10.1016/j.neuro.2020.08.005",
+  },
+  {
+    id: 7,
+    title:
+      "Neurotoxicity associated with chimeric antigen receptor T-cell therapy.",
+    author:
+      "Wronski, M., Wesselingh, R., Kazzi, C., Seery, N., Ko, K., Li, J., Tan, T. H., Simpson, T., Abbott, C., Fleming, S., Inam, S., Tam, C. S., Wong, S. M., O'Brien, T., van der Walt, A., Spencer, A., Butzkueven, H., & Monif, M.",
+    date: "2025",
+    link: "https://doi.org/10.1016/j.jneuroim.2025.578717",
+  },
+  {
+    id: 8,
+    title:
+      "Glutamate Neurotoxicity and Destruction of the Blood-Brain Barrier: Key Pathways for the Development of Neuropsychiatric Consequences of TBI and Their Potential Treatment Strategies.",
+    author:
+      "Gruenbaum, B. F., Zlotnik, A., Fleidervish, I., Frenkel, A., & Boyko, M.",
+    date: "2022",
+    link: "https://doi.org/10.3390/ijms23179628",
+  },
+  {
+    id: 9,
+    title:
+      "Melatonin enhances the restoration of neurological impairments and cognitive deficits during drug withdrawal in methamphetamine-induced toxicity and endoplasmic reticulum stress in rats.",
+    author:
+      "Komlao, P., Kraiwattanapirom, N., Promyo, K., Hein, Z. M., & Chetsawang, B",
+    date: "2023",
+    link: "https://doi.org/10.1016/j.neuro.2023.11.006",
+  },
+  {
+    id: 10,
+    title:
+      "Glial pathology and retinal neurotoxicity in the anterior visual pathway in experimental autoimmune encephalomyelitis.",
+    author:
+      "Jin, J., Smith, M. D., Kersbergen, C. J., Kam, T. I., Viswanathan, M., Martin, K., Dawson, T. M., Dawson, V. L., Zack, D. J., Whartenby, K., & Calabresi, P. A.",
+    date: "2019",
+    link: "https://doi.org/10.1186/s40478-019-0767-6",
+  },
+];
+
+export default publications;
--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -1,9 +1,78 @@
+import { NavLink } from "react-router-dom";
+
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import publicationsData from "../data/publications-data.js";
+
 function Publications() {
+  const columns = [
+    { accessorKey: "title", header: "Title" },
+    { accessorKey: "author", header: "Author" },
+    { accessorKey: "date", header: "Date" },
+    { accessorKey: "link", header: "Link" },
+  ];
+
+  const table = useReactTable({
+    data: publicationsData,
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <div className="publications">
       <div className="publications-page">
-        <div className="hero"></div>
-        <div className="content"></div>
+        <div className="hero">
+          <h1>PUBLICATIONS</h1>
+          <br></br>
+          <p>
+            The following publications are referenced for illustrative purposes
+            only. Neurolytica Labs is a fictional organisation created as a web
+            development portfolio project. All papers are the work of their
+            respective authors.
+          </p>
+          <br></br>
+        </div>
+        <div className="content">
+          <table>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <br></br>
+        <div className="publications-contact">
+          <NavLink to="/contact">
+            <button id="collaborate">Collaborate with us</button>
+          </NavLink>
+        </div>
         <div className="footer"></div>
       </div>
     </div>
